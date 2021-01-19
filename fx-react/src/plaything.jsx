7c3cdf09a2c4bc4686eaa6dd26380e654be49fba
nazeer171 <nazeer3886@gmail.com>
2021-01-19 01:53:43 -0600

    added search bar for filtering currencies

--- fx-react/src/plaything.jsx
+++ fx-react/src/plaything.jsx
@@ -1,59 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Chart } from "react-google-charts";
 import CandleStick from "./Data/CandleStick.json"
 
 const Plaything = ({ infoName }) => {
 
-  const stick2 = Object.keys(CandleStick[0])[1]
-
-  const time2 = CandleStick[0][stick2].tm.split(" ")[1]
-  const date2 = CandleStick[0][stick2].tm.split(" ")[0]
-  const open2 = CandleStick[0][stick2].o
-  const high2 = CandleStick[0][stick2].h
-  const close2 = CandleStick[0][stick2].c
-  const low2 = CandleStick[0][stick2].l
-
-  console.log(stick2)
-
-  // let time;
-  // let open;
-  // let high;
-  // let close;
-  // let low;
-
-  const stick1 = Object.keys(CandleStick[0])
-  console.log(stick1)
-
-  const candleArray = () => stick1.map(candy => {
-  console.log(candy)
-  return(
-    // time = CandleStick[0][stick1].tm.split(" ")[1],
-    // open = CandleStick[0][stick1].o,
-    // high = CandleStick[0][stick1].h,
-    // close = CandleStick[0][stick1].c,
-    // low = CandleStick[0][stick1].l
-    "spread love"
-    )
-})
-
-  const drawCandleSticks = () => {
-    console.log(Object.keys(CandleStick[0]))
-    return [time2, low2, open2, close2, high2]
-  }
-  drawCandleSticks()
-
-  
-
-
-  const [data, setData] = useState(["time", "low", "open", "close", "high"])
-
-  const initialState = { dataLoadingStatus: 'loading', chartData: [] }
-  const rateCurrencyNames = Object.keys(CandleStick[0])
-    const rateCurrencyValues = Object.values(CandleStick[0])
-    const chartData = [['Currency Name', 'Currency Rate']]
-    for (let i = 0; i < rateCurrencyNames.length; i++ ) {
-      chartData.push([rateCurrencyNames[i], rateCurrencyValues[i]])
-    }
+  const arr = Object.entries(CandleStick[0])
+  const chartdata = arr.map(kv => [ kv[1]['tm'].split(" ")[1], parseFloat(kv[1]['l']) ,parseFloat(kv[1]['o']) , parseFloat(kv[1]['c']) , parseFloat(kv[1]['h']) ])
 
   return (
     <Chart
@@ -61,12 +13,9 @@ const Plaything = ({ infoName }) => {
       height={"100%"}
       chartType="CandlestickChart"
       loader={<div>Loading Chart</div>}
-      data={[
-        ["time", "low", "open", "close", "high"],
-        ["08:00:00", 1.22535, 1.22665, 1.2259, 1.227],
-        [time2, low2, open2, close2, high2],
-        // [candleArray(time), candleArray(low), candleArray(open), candleArray(close), candleArray(high)]
-
+      data={
+        [["time", "low", "open", "close", "high"],
+        ...chartdata
       ]}
       options={
         // Chart options
@@ -82,11 +31,11 @@ const Plaything = ({ infoName }) => {
               fontSize: 25,
               color: "green",
             },
-            viewWindow: { min: 0, max: 6 },
+            viewWindow: { min: 0, max: 30 },
           },
           vAxis: {
             title: "Price",
-            viewWindow: { min: 1.225, max: 1.23 },
+            viewWindow: { min: 1.225, max: 1.231 },
             titleTextStyle: {
               fontSize: 25
             }
